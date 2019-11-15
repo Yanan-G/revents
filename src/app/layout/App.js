f8125ae684aca0f5efca0b4737b5e532c3dc3bdb
Yanan Geng <justine@Yanans-MacBook-Air.local>
2019-11-14 19:47:12 -0500

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const App = () => (
+	<div className="App">
+		<h1> </h1>
+		<Button icon="smile" content="react btn" />
+	</div>
+)
+
+export default App
